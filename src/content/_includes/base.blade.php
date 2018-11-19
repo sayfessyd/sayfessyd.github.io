25be94bfd9f040b1ae991e736be90b740664525d
sayfessyd <mail.sayf@yahoo.com>
2018-11-19 22:34:35 +0100

    commit

--- src/content/_includes/base.blade.php
+++ src/content/_includes/base.blade.php
@@ -12,7 +12,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-
+        <meta name="theme-color" content="#4B6A78">
+        
         <!-- Google Font: Lato -->
         <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link href="https://fonts.googleapis.com/css?family=Poppins:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext" rel="stylesheet">
