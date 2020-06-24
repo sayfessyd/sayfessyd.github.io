d2477101df59e1760385ab6d71d0883173a6e7f4
Saifeddine Essid <mail.sayf@yahoo.com>
2020-06-24 20:17:13 +0100

    commit

--- config.php
+++ config.php
@@ -71,6 +71,7 @@ return [
         ['title' => 'Software Engineer', 'time' => "2019 - Present", 'company' => 'Be-softilys', 'details' => 'Developed a system that allows logs management with Blockchain technology. Working on a software product using Spring Boot and Angular technologies. Developing a number of DevOps scripts.', 'location' => 'Lac 3, TN'],
         ['title' => 'Software Engineer', 'time' => "2019 - Present", 'company' => 'Widgetic', 'details' => 'Work on embed interactive widgets for potential customers. Create applications for the no-code community available on a software marketplace for non-coders.', 'location' => 'Remote, WLD'],
     ],
+
     'expertise' => [
         ['name' => 'Frontend', 'details' => 'Responsible for implementing visual and interactive elements. Use Vue.js, Angular, and Svelte as front-end frameworks in recent projects.'],
         ['name' => 'DevOps', 'details' => 'Use Docker and Ansible to create DevOps scripts for deploying third-party applications. Use Gitlab CI, Travis CI as continuous integration tools.'],
@@ -81,22 +82,27 @@ return [
         ['name' => 'Machine Learning', 'details' => 'Responsible for designing and developing machine learning and deep learning systems. Use Tensorflow.js as a high-level framework for training models.'],
         ['name' => 'Architecture', 'details' => 'Make high-level design choices and tries to enforce technical standards, including software coding standards, tools, and platforms.']
     ],
+
     'worldwide' => [
         ['href' => 'https://www.ieee.org', 'title' => 'IEEE Member', 'time' => "2016", 'company' => 'IEEE Tunisia Section', 'details' => "A member of the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity.", 'location'=>'Ariana']
     ],
+
     'organizations' => [
         ['href' => 'https://github.com/specialappdev', 'company' => 'SpecialAppDev', 'details' => "Our team focus on creating innovative Hybrid applications, JavaScript plugins, PHP platforms, and WooCommerce themes."],
         ['href' => 'https://github.com/assmajs', 'company' => 'Assma.js', 'details' => "Compared to Angular, React, and Vue, it combines all the positive aspects of these popular libraries into one small package."],
     ],
+
     'projects' => [
         ['title' => 'Message.Me', 'description' => "MessageMe is a social media script, the ONLY ONE of its kind based on anonymous messages and built on top of Laravel 5.4 with all possible features.", 'url' => "https://codecanyon.net/item/messageme-laravel-anonymous-social-media-script/22280893", 'image' => 'messageme.jpg'],
         ['title' => 'Lollipop 2.0 (Premium Software / JavaScript plugin)', 'description' => "Lollipop 2.0 is built using the latest HTML5, CSS3, and JavaScript techniques/tools.", 'url' => "https://codecanyon.net/item/lollipop-image-editor/12957420", 'image' => 'lollipop.jpg'],
     ],
+
     'education' => [
         ['title' => "Engineer's Degree in Software Engineering", 'university' => 'Higher Institute Of Computer Science', 'period' => '2016 - 2019', 'location' => 'Ariana, TN', 'details' => "Took courses in programming language theory, information theory, complexity theory, probability, computer architecture, distributed computing, computer networks, artificial intelligence, operating systems, software design, software development."],
         ['title' => "Licentiate's Degree in Computer Systems", 'university' => 'Higher Institute Of Computer Science', 'period' => '2013 - 2016', 'location' => 'Ariana, TN', 'details' => "Took courses in algorithm design, operating systems, introduction to software engineering, programming on the web, introduction to databases, introduction to computer networks."],
         ['title' => "School Baccalaureate's Degree in Natural Sciences", 'university' => 'Ibn Rochd High School', 'period' => '2012 - 2013', 'location' => 'Menzel Bourguiba, TN', 'details' => "Took courses in natural sciences, mathematics, physics, chemistry, literature."]
     ],
+
     'profiles' => [
         'linkedin' => 'https://www.linkedin.com/in/saifeddine-essid-539a32a4',
         'github' => 'https://github.com/sayfessyd',
@@ -110,6 +116,7 @@ return [
         'medium' => 'https://medium.com/@sayfessyd',
         'blog' => 'https://codebase.sayfessyd.com'
     ],
+
     'skills' => [
         ['title' => 'Vue.js & Angular & Svelte', 'level' => '90', 'count' => '3'],
         ['title' => 'Java & Spring Boot', 'level' => '80', 'count' => '2'],
@@ -125,4 +132,5 @@ return [
         ['title' => 'Android & Apache Cordova', 'level' => '60', 'count' => '2'],
         ['title' => 'VB & ASP.NET', 'level' => '60', 'count' => '2'],
     ],
+
 ];
